f2ad2c7a3e02b6abbe1bc37e5d8e91f772147811
Rhyse Simpson <skat1@live.com.au>
2018-10-24 01:02:52 +1100

    format code in install.sh

--- common/install.sh
+++ common/install.sh
@@ -1,4 +1,8 @@
-if [ -f $DATADIR/ch.deletescape.lawnchair.dev*/base.apk ] || [ -f $DATADIR/ch.deletescape.lawnchair.ci*/base.apk ] || [ -f $DATADIR/ch.deletescape.lawnchair.plah*/base.apk ] || [ -f $DATADIR/ch.deletescape.lawnchair-*/base.apk ] || [ -f $SYSDIR/Lawnchair*/Lawnchair*.apk ]; then
+if [ -f $DATADIR/ch.deletescape.lawnchair.dev*/base.apk ] \
+  || [ -f $DATADIR/ch.deletescape.lawnchair.ci*/base.apk ] \
+  || [ -f $DATADIR/ch.deletescape.lawnchair.plah*/base.apk ] \
+  || [ -f $DATADIR/ch.deletescape.lawnchair-*/base.apk ] \
+  || [ -f $SYSDIR/Lawnchair*/Lawnchair*.apk ]; then
   for var in $VARS; do
     case $(grep -F "$var" "$FILE") in
       *ch.deletescape.lawnchair.dev*)
@@ -10,9 +14,11 @@ if [ -f $DATADIR/ch.deletescape.lawnchair.dev*/base.apk ] || [ -f $DATADIR/ch.de
         fi
         cp_ch $STEP/LawnstepDev.apk $STEPDIR/Lawnstep-Dev.apk
         sed -i "s|<VAR>|$var|" $PERM/privapp-permissions-lawnchair.xml
-        sed -i "s|<VAR>|$var|" $CONF/lawnchair-hiddenapi-package-whitelist.xml
-        cp_ch $PERM/privapp-permissions-lawnchair.xml $PERMDIR/privapp-permissions-lawnchair-dev.xml
-        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml $CONFDIR/lawnchair-dev-hiddenapi-package-whitelist.xml
+        sed -i "s|<VAR>|$var|g" $CONF/lawnchair-hiddenapi-package-whitelist.xml
+        cp_ch $PERM/privapp-permissions-lawnchair.xml \
+          $PERMDIR/privapp-permissions-lawnchair-dev.xml
+        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml \
+          $CONFDIR/lawnchair-dev-hiddenapi-package-whitelist.xml
         break;;
       *ch.deletescape.lawnchair.ci*)
         ui_print "   $var detected!"
@@ -23,9 +29,11 @@ if [ -f $DATADIR/ch.deletescape.lawnchair.dev*/base.apk ] || [ -f $DATADIR/ch.de
         fi
         cp_ch $STEP/LawnstepCi.apk $STEPDIR/Lawnstep-Ci.apk
         sed -i "s|<VAR>|$var|" $PERM/privapp-permissions-lawnchair.xml
-        sed -i "s|<VAR>|$var|" $CONF/lawnchair-hiddenapi-package-whitelist.xml
-        cp_ch $PERM/privapp-permissions-lawnchair.xml $PERMDIR/privapp-permissions-lawnchair-ci.xml
-        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml $CONFDIR/lawnchair-ci-hiddenapi-package-whitelist.xml
+        sed -i "s|<VAR>|$var|g" $CONF/lawnchair-hiddenapi-package-whitelist.xml
+        cp_ch $PERM/privapp-permissions-lawnchair.xml \
+          $PERMDIR/privapp-permissions-lawnchair-ci.xml
+        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml \
+          $CONFDIR/lawnchair-ci-hiddenapi-package-whitelist.xml
         break;;
       *ch.deletescape.lawnchair.plah*)
         ui_print "   $var detected!"
@@ -36,9 +44,11 @@ if [ -f $DATADIR/ch.deletescape.lawnchair.dev*/base.apk ] || [ -f $DATADIR/ch.de
         fi
         cp_ch $STEP/LawnstepPlah.apk $STEPDIR/Lawnstep-Plah.apk
         sed -i "s|<VAR>|$var|" $PERM/privapp-permissions-lawnchair.xml
-        sed -i "s|<VAR>|$var|" $CONF/lawnchair-hiddenapi-package-whitelist.xml
-        cp_ch $PERM/privapp-permissions-lawnchair.xml $PERMDIR/privapp-permissions-lawnchair-plah.xml
-        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml $CONFDIR/lawnchair-plah-hiddenapi-package-whitelist.xml
+        sed -i "s|<VAR>|$var|g" $CONF/lawnchair-hiddenapi-package-whitelist.xml
+        cp_ch $PERM/privapp-permissions-lawnchair.xml \
+          $PERMDIR/privapp-permissions-lawnchair-plah.xml
+        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml \
+          $CONFDIR/lawnchair-plah-hiddenapi-package-whitelist.xml
         break;;
       *'ch.deletescape.lawnchair '*)
         ui_print "   $var detected!"
@@ -49,9 +59,11 @@ if [ -f $DATADIR/ch.deletescape.lawnchair.dev*/base.apk ] || [ -f $DATADIR/ch.de
         fi
         cp_ch $STEP/Lawnstep.apk $STEPDIR/Lawnstep.apk
         sed -i "s|<VAR>|$var|" $PERM/privapp-permissions-lawnchair.xml
-        sed -i "s|<VAR>|$var|" $CONF/lawnchair-hiddenapi-package-whitelist.xml
-        cp_ch $PERM/privapp-permissions-lawnchair.xml $PERMDIR/privapp-permissions-lawnchair.xml
-        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml $CONFDIR/lawnchair-hiddenapi-package-whitelist.xml
+        sed -i "s|<VAR>|$var|g" $CONF/lawnchair-hiddenapi-package-whitelist.xml
+        cp_ch $PERM/privapp-permissions-lawnchair.xml \
+          $PERMDIR/privapp-permissions-lawnchair.xml
+        cp_ch $CONF/lawnchair-hiddenapi-package-whitelist.xml \
+          $CONFDIR/lawnchair-hiddenapi-package-whitelist.xml
         break;;
     esac
     rm /data/resource-cache/overlays.list
